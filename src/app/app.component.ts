57b608c8f0995863cb9dafcf90e17e1528ebf506
Henriksveen <simen.henriksveen@safebase.no>
2019-12-04 13:31:49 +0100

    ws-example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Select, Store} from '@ngxs/store';
+import {KafkaState} from './state/kafka.state';
+import {Observable} from 'rxjs';
+import {ConnectWebSocket} from '@ngxs/websocket-plugin';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  @Select(KafkaState.messages)
+  kafkaMessages$: Observable<string>;
+
+  constructor(private store: Store) {
+  }
+
+  ngOnInit() {
+    this.store.dispatch(new ConnectWebSocket());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgxsWebsocketPluginModule} from '@ngxs/websocket-plugin';
+import {KafkaState} from './state/kafka.state';
+import {NgxsModule} from '@ngxs/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([
+      KafkaState
+    ]),
+    NgxsWebsocketPluginModule.forRoot({
+      url: 'ws://localhost:8080/websocket'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
